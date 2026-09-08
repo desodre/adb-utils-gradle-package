0b243a1821e50cbfabeb509b29b9017ef29cb7b4
desodre <jhonathacirilo9000@gmail.com>
2026-09-07 20:27:56 -0400

    build: configure Kotlin JVM library version 0.1.0

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.4.10"
+    `java-library`
+}
+
+group = "org.desodre"
+version = "0.1.0"
+
+repositories { mavenCentral() }
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    testImplementation(kotlin("test-junit5"))
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin { jvmToolchain(21) }
+tasks.test { useJUnitPlatform() }
